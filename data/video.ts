0387d43e559c93f13d6984aec03cc6cd5b07ec4c
David Odeyinka <odeyinkadavid@gmail.com>
2026-01-04 21:15:18 -0500

    Hardcode dummy data based on video item type

---
+++ data/video.ts
@@ -0,0 +1,28 @@
+import { VideoItem } from "../types/video";
+
+export const VIDEOS: VideoItem[] = [
+  {
+    id: "1",
+    title: "Global Markets React to Inflation Data",
+    category: "Business",
+    thumbnail: "https://picsum.photos/400/250?1",
+    source:
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+  },
+  {
+    id: "2",
+    title: "Breaking: Election Results Update",
+    category: "Politics",
+    thumbnail: "https://picsum.photos/400/250?2",
+    source:
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+  },
+  {
+    id: "3",
+    title: "Tech Giants Announce New AI Push",
+    category: "Tech",
+    thumbnail: "https://picsum.photos/400/250?3",
+    source:
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+  },
+];
