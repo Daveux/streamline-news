84983f90b41e5034e7f7079ad1257209b8aa96dc
David Odeyinka <odeyinkadavid@gmail.com>
2026-01-04 21:23:07 -0500

    Setup entry point to glue everything together

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { VideoItem } from "./types/video";
+import { HomeScreen } from "./screens/HomeScreen";
+import { PlayerScreen } from "./screens/PlayerScreen";
 
 export default function App() {
+  const [selectedVideo, setSelectedVideo] = useState<VideoItem | null>(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      {selectedVideo ? (
+        <PlayerScreen
+          video={selectedVideo}
+          onExit={() => setSelectedVideo(null)}
+        />
+      ) : (
+        <HomeScreen onSelectVideo={setSelectedVideo} />
+      )}
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
